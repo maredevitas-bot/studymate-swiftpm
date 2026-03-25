21bcb8dbf923c117979254ea6e79af9af3612005
StudyMate <studymate@dev.local>
2026-03-25 19:53:04 +0900

    update: 2026-03-25 19:53

--- Sources/StudyMateApp.swift
+++ Sources/StudyMateApp.swift
@@ -3,17 +3,36 @@ import SwiftData
 
 @main
 struct StudyMateApp: App {
+    let container: ModelContainer
+
+    init() {
+        let schema = Schema([
+            School.self, Subject.self, Material.self,
+            QuizSession.self, Question.self, PlanEntry.self
+        ])
+        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            container = try ModelContainer(for: schema, configurations: [config])
+        } catch {
+            // 스키마 변경으로 마이그레이션 실패 시 기존 스토어 삭제 후 재생성
+            if let url = config.url {
+                try? FileManager.default.removeItem(at: url)
+                let walURL = url.deletingLastPathComponent()
+                    .appendingPathComponent(url.lastPathComponent + "-wal")
+                let shmURL = url.deletingLastPathComponent()
+                    .appendingPathComponent(url.lastPathComponent + "-shm")
+                try? FileManager.default.removeItem(at: walURL)
+                try? FileManager.default.removeItem(at: shmURL)
+            }
+            container = try! ModelContainer(for: schema, configurations: [config])
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: [
-            School.self,
-            Subject.self,
-            Material.self,
-            QuizSession.self,
-            Question.self,
-            PlanEntry.self
-        ])
+        .modelContainer(container)
     }
 }
