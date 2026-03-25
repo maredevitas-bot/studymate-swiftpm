f8d162b61beb00c833224f764870f20e2cd5ad36
StudyMate <studymate@dev.local>
2026-03-25 19:44:36 +0900

    update: 2026-03-25 19:44

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 import AppleProductTypes
@@ -31,7 +31,10 @@ let package = Package(
     targets: [
         .executableTarget(
             name: "StudyMate",
-            path: "Sources"
+            path: "Sources",
+            swiftSettings: [
+                .swiftLanguageVersion(.v5)
+            ]
         )
     ]
 )
